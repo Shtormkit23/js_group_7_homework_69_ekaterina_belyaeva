3ffb594c2bb18d4b40dfd2e3b7807b3062685fca
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2020-08-03 15:25:42 +0600

    Updated dependencies, removed class component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,12 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 
-class BurgerBuilder extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>Burger will be here</div>
-        <div>Build controls will be here</div>
-      </Fragment>
-    )
-  }
-}
+const BurgerBuilder = () => {
+  return (
+    <Fragment>
+      <div>Burger will be here</div>
+      <div>Build controls will be here</div>
+    </Fragment>
+  )
+};
 
 export default BurgerBuilder;
