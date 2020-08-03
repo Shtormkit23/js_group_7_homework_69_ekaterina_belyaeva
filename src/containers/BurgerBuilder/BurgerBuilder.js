eb4d9e0ed17da396b380bbe9207aeff3532bcdce
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2020-08-03 15:42:23 +0600

    Changed Fragment to empty tag

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 const BurgerBuilder = () => {
   return (
-    <Fragment>
+    <>
       <div>Burger will be here</div>
       <div>Build controls will be here</div>
-    </Fragment>
+    </>
   )
 };
 
