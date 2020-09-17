b9b7efdb25908a59e520ba32957fbe6eb05d29fd
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-09-17 17:54:40 +0600

    Добавлена связь хранилища с приложением

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import thunkMiddleware from 'redux-thunk'
+import reducer from "./store/reducer";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
